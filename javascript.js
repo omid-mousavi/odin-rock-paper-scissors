00a0c4ac0688499a14ea5f61de970d2824b4bf18
omid-mousavi <omidmousavi266@gmail.com>
2024-02-14 14:20:23 +0330

    capitilizing first letter and playRound and playGame function
    i used string methods in able to capitilze user choice text, which will be not case sensitive, and added playRound and playGame boilerplate

--- javascript.js
+++ javascript.js
@@ -2,10 +2,19 @@ function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * 3) + 1; // math random result is between 0 and 1, then multipied by 3, result will be between 0 and 3(exclusive), i used floor method to round it and incrementing it will be between 1 to 3(inclusive)
   switch(randomChoice) {
     case 1:
-      return "rock";
+      return "Rock";
     case 2:
-      return "paper";
+      return "Paper";
     case 3:
-      return "scissors";
+      return "Scissors";
   }
+}
+function capitilizeFirstLetter(yourWord) {
+  return `${yourWord.toUpperCase().at(0)}${yourWord.toLowerCase().slice(1)}`;
+}
+function playRound(playerSelection, computerSelection) {
+  playerSelection = capitilizeFirstLetter(playerSelection);
+}
+function playGame() {
+  
 }
